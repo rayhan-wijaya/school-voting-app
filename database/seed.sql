335ddeebea03401b108dba93aa91c7d961c21404
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-08-30 00:42:56 +0700

    created seed.sql

---
+++ database/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO
+    `organization_member` (`organization_id`, `nickname`, `full_name`, `position`)
+VALUES
+    (1, 'Haykall', 'Full Name Test', 'vice_chairman'),
+    (1, 'Sasha', 'Full Name Test', 'chairman'),
+    (1, 'Name', 'Full Name Test', 'vice_chairman'),
+    (1, 'Another Name', 'Full Name Test', 'chairman');
