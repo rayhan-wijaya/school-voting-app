9b326ebc1b57ec90e77d46033dd66a34d355a7f4
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-08-30 01:19:35 +0700

    adjusted env.ts to suit next.js

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,16 +1,18 @@
-import { createEnv } from "@t3-oss/env-core";
+import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
+    client: {
+        NEXT_PUBLIC_BASE_URL: z.string(),
+    },
     server: {
-        BASE_URL: z.string(),
         DATABASE_HOST: z.string(),
         DATABASE_USER: z.string(),
         DATABASE_PASSWORD: z.string(),
         DATABASE_NAME: z.string(),
     },
-    runtimeEnvStrict: {
-        BASE_URL: process.env.BASE_URL,
+    runtimeEnv: {
+        NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
         DATABASE_HOST: process.env.DATABASE_HOST,
         DATABASE_USER: process.env.DATABASE_USER,
         DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
