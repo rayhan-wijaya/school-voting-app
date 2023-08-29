338ff803eef9dbf72fccb29b6a75cb44365cf18f
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-08-30 00:23:53 +0700

    created database object

---
+++ src/lib/database.js
@@ -0,0 +1,12 @@
+import { createPool } from "mysql";
+import { env } from "./env.js";
+
+export const database = {
+    pool: createPool({
+        connectionLimit: 10,
+        host: env.DATABASE_HOST,
+        user: env.DATABASE_USER,
+        password: env.DATABASE_PASSWORD,
+        database: env.DATABASE_NAME,
+    }),
+};
