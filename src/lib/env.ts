55e458d257e362ba072cbb389512627ab9c4938e
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-08-30 01:15:18 +0700

    created BASE_URL

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,12 +3,14 @@ import { z } from "zod";
 
 export const env = createEnv({
     server: {
+        BASE_URL: z.string(),
         DATABASE_HOST: z.string(),
         DATABASE_USER: z.string(),
         DATABASE_PASSWORD: z.string(),
         DATABASE_NAME: z.string(),
     },
     runtimeEnvStrict: {
+        BASE_URL: process.env.BASE_URL,
         DATABASE_HOST: process.env.DATABASE_HOST,
         DATABASE_USER: process.env.DATABASE_USER,
         DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
