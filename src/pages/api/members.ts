15cd59a834eadda6749ac85360781118bd7d0a7a
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-08-30 01:00:11 +0700

    created getAllMembers() function

--- src/pages/api/members.ts
+++ src/pages/api/members.ts
@@ -11,3 +11,46 @@ const organizationMemberSchema = z.object({
 });
 
 type OrganizationMember = z.infer<typeof organizationMemberSchema>;
+
+async function getAllMembers() {
+    return new Promise<OrganizationMember[]>(function (resolve, reject) {
+        database.pool.query(
+            `
+                SELECT
+                    id,
+                    organization_id as organizationId,
+                    nickname,
+                    full_name as fullName,
+                    position
+                FROM organization_member;
+            `,
+            async function (error, results, _fields) {
+                if (error) {
+                    return reject(error);
+                }
+
+                if (!Array.isArray(results)) {
+                    return reject("`results` wasn't an array");
+                }
+
+                const memberResults = await Promise.allSettled(
+                    results.map(async function (result) {
+                        return organizationMemberSchema.parseAsync(result);
+                    })
+                );
+
+                const members = memberResults
+                    .filter(function (
+                        memberResult
+                    ): memberResult is PromiseFulfilledResult<OrganizationMember> {
+                        return memberResult.status === "fulfilled";
+                    })
+                    .map(function (memberResult) {
+                        return memberResult.value;
+                    });
+
+                return resolve(members);
+            }
+        );
+    });
+}
