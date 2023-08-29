584bdd6fe30be96e6a74945c0f591c2f9aa28af1
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-08-30 00:59:59 +0700

    created OrganizationMember schema

--- src/pages/api/members.ts
+++ src/pages/api/members.ts
@@ -1,3 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
 import { database } from "~/lib/database";
+
+const organizationMemberSchema = z.object({
+    id: z.number(),
+    organizationId: z.number(),
+    nickname: z.string(),
+    fullName: z.string(),
+    position: z.enum(["chairman", "vice_chairman"]),
+});
+
+type OrganizationMember = z.infer<typeof organizationMemberSchema>;
