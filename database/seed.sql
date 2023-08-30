fd3dc5151a2e4089660b641fee1f391f0090ddfc
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-08-30 17:27:25 +0700

    serious seed data

--- database/seed.sql
+++ database/seed.sql
@@ -1,7 +1,7 @@
 INSERT INTO
     `organization_member` (`organization_id`, `nickname`, `full_name`, `position`)
 VALUES
-    (1, 'Haykall', 'Full Name Test', 'vice_chairman'),
-    (1, 'Sasha', 'Full Name Test', 'chairman'),
-    (1, 'Name', 'Full Name Test', 'vice_chairman'),
-    (1, 'Another Name', 'Full Name Test', 'chairman');
+    (1, 'Rayhan', 'Rayhan Satria Wijaya', 'vice_chairman'),
+    (1, 'Malik', 'Malik Syarif Akbar', 'chairman'),
+    (1, 'Akesh', 'Gede Akesh', 'vice_chairman'),
+    (1, 'Daffa', 'Daffa Prawono', 'chairman');
