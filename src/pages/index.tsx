1cc07ff2999dcc15ad0456d9c689775e7fc969d5
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-08-31 21:03:11 +0700

    used chevron arrows

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -169,7 +169,20 @@ export default function Home(
                     });
                 }}
             >
-                Prev
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M15.75 19.5L8.25 12l7.5-7.5"
+                    />
+                </svg>
             </button>
 
             <button
@@ -183,7 +196,20 @@ export default function Home(
                     });
                 }}
             >
-                Next
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                    />
+                </svg>
             </button>
         </div>
     );
