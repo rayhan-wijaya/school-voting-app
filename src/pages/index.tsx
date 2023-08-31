39498637dcd7d6ed0e8f27d98314fb4b483b0731
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-08-31 23:32:37 +0700

    stylized submit button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -193,9 +193,29 @@ export default function Home(
                     ) : null}
 
                     {pageIndex === pageIndexLimit ? (
-                        <div>
-                            <button>Submit</button>
-                        </div>
+                        <>
+                            <div className="p-8" />
+
+                            <div className="flex gap-3 items-center justify-center">
+                                <button className="bg-gray-400 text-white font-semibold px-6 py-3 rounded-xl flex gap-3 justify-center items-center">
+                                    Submit
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth="1.5"
+                                        stroke="currentColor"
+                                        className="w-6 h-6"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                                        />
+                                    </svg>
+                                </button>
+                            </div>
+                        </>
                     ) : null}
 
                     <div className="p-3" />
