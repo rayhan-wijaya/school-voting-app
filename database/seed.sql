45ae09c9cbeab33aed17aae4028883e021fd24ca
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-04 00:29:18 +0700

    updated seed data accordingly

--- database/seed.sql
+++ database/seed.sql
@@ -1,23 +1,24 @@
 INSERT INTO
-    `organization_pair` (`organization_id`)
+    `organization_pair` (`organization_id`, `pair_id`)
 VALUES
-    (1),
-    (1),
-    (2),
-    (2);
+    (1, 1),
+    (1, 2),
+
+    (2, 1),
+    (2, 2);
 
 INSERT INTO
-    `organization_member` (`pair_id`, `nickname`, `full_name`, `position`)
+    `organization_member` (`organization_id`, `pair_id`, `nickname`, `full_name`, `position`)
 VALUES
-    (1, 'Rayhan', 'Rayhan Satria Wijaya', 'vice_chairman'),
-    (1, 'Malik', 'Malik Syarif Akbar', 'chairman'),
-    (2, 'Akesh', 'Gede Akesh', 'vice_chairman'),
-    (2, 'Daffa', 'Daffa Prawono', 'chairman'),
+    (1, 1, 'Rayhan', 'Rayhan Satria Wijaya', 'vice_chairman'),
+    (1, 1, 'Malik', 'Malik Syarif Akbar', 'chairman'),
+    (1, 2, 'Akesh', 'Gede Akesh', 'vice_chairman'),
+    (1, 2, 'Daffa', 'Daffa Prawono', 'chairman'),
 
-    (3, 'Rayhan', 'Rayhan Satria Wijaya', 'vice_chairman'),
-    (3, 'Malik', 'Malik Syarif Akbar', 'chairman'),
-    (4, 'Akesh', 'Gede Akesh', 'vice_chairman'),
-    (4, 'Daffa', 'Daffa Prawono', 'chairman');
+    (2, 1, 'Rayhan', 'Rayhan Satria Wijaya', 'vice_chairman'),
+    (2, 1, 'Malik', 'Malik Syarif Akbar', 'chairman'),
+    (2, 2, 'Akesh', 'Gede Akesh', 'vice_chairman'),
+    (2, 2, 'Daffa', 'Daffa Prawono', 'chairman');
 
 INSERT INTO
     `student` (`id`, `full_name`, `hashed_password`)
