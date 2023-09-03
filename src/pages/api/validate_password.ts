15c4b972e5d0e077ec933e342f1f9ec7d4801970
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-03 22:33:34 +0700

    recreated numericString schema

--- src/pages/api/validate_password.ts
+++ src/pages/api/validate_password.ts
@@ -1,3 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { isAuthValid } from "~/lib/auth";
 import { z } from "zod";
+
+const numericString = z
+    .string()
+    .regex(/^\d+$/)
+    .transform(function (value) {
+        return Number(value);
+    });
