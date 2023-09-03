065fa3580bb7f83f24d41a3e1d38df0356004cf4
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-03 21:11:53 +0700

    included <Header /> in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import "~/styles/globals.css";
 import type { AppProps } from "next/app";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Header from "~/components/Header";
 
 const client = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <QueryClientProvider client={client}>
+            <Header />
             <Component {...pageProps} />
         </QueryClientProvider>
     );
