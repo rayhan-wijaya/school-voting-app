aeccc591f47749e5bbd9482e86ef2a627da1b8c9
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-03 23:06:44 +0700

    imported useEffect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import type { OrganizationMembers } from "~/pages/api/members";
 import { InferGetStaticPropsType } from "next";
