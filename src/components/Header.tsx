8b5ffb588319856f758af306f417f1dc41e9f905
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-03 21:12:13 +0700

    created <Header />

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <div>
+            <div className="font-semibold bg-gray-600 px-3 py-5 text-white">OSIS / MPK Voting</div>
+        </div>
+    );
+}
+
+export default Header;
