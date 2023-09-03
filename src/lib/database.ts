756f9cabdbb005a2173d60195dabdb06e5021559
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-04 01:39:45 +0700

    increased limit to 2000

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -3,7 +3,7 @@ import { env } from "./env";
 
 export const database = {
     pool: createPool({
-        connectionLimit: 500,
+        connectionLimit: 2000,
         host: env.DATABASE_HOST,
         user: env.DATABASE_USER,
         password: env.DATABASE_PASSWORD,
