235d836b14e2939056954db0f679e857d32818ec
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-03 13:09:34 +0700

    removed styles from <RadioGroup />

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -130,7 +130,7 @@ function VotePage({
 
                                     return (
                                         <RadioGroup.Option
-                                            className="focus:outline-none ui-checked:bg-gray-500 bg-gray-100 rounded-xl flex flex-col gap-3 ui-checked:divide-gray-600 divide-y ui-checked:text-white cursor-pointer ui-active:ring-4 ui-active:ring-gray-400"
+                                            className="focus:outline-none ui-checked:bg-gray-500 bg-gray-100 rounded-xl ui-checked:text-white cursor-pointer ui-active:ring-4 ui-active:ring-gray-400 shadow-md"
                                             value={pairId}
                                         >
                                             {pair.map(function (member) {
