18e4c1798602f633bafa16d0894a7795ae21504c
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-03 22:18:21 +0700

    created passwordResultsSchema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,9 @@
 import { z } from "zod";
 import { createHash, timingSafeEqual } from "crypto";
 import { database } from "./database";
+
+const passwordResultsSchema = z.array(
+    z.object({
+        password: z.string(),
+    })
+);
