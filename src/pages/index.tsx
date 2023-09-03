3de32de4be48d7c6165e43883719b38e37919442
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-04 01:32:29 +0700

    relayouted flex container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -135,7 +135,7 @@ function VotePage({
     const organizationNames = Object.keys(members);
 
     return (
-        <div className="flex flex-col gap-16">
+        <div>
             <div>
                 <h1 className="text-center font-semibold">
                     Pick your Vote for Each Organization
@@ -146,103 +146,110 @@ function VotePage({
                 <p className="text-center">Please cast your votes</p>
             </div>
 
-            {organizationNames.map(function (organizationName) {
-                const organizationPairs = members[organizationName];
-                const pairIds = Object.keys(organizationPairs);
+            <div className="p-6" />
 
-                return (
-                    <div>
-                        <h2 className="font-semibold text-center">
-                            {organizationName}
-                        </h2>
+            <div className="flex flex-col gap-16">
+                {organizationNames.map(function (organizationName) {
+                    const organizationPairs = members[organizationName];
+                    const pairIds = Object.keys(organizationPairs);
 
-                        <div className="p-3" />
+                    return (
+                        <div>
+                            <h2 className="font-semibold text-center">
+                                {organizationName}
+                            </h2>
 
-                        <RadioGroup
-                            by={"organizationPairCompositeId"}
-                            onChange={function (pair: OrganizationPair) {
-                                setOrganizationPairs(function (prevPairs) {
-                                    return {
-                                        ...prevPairs,
-                                        [organizationName]: pair,
-                                    };
-                                });
-                            }}
-                        >
-                            <div className="grid grid-cols-2 gap-5">
-                                {pairIds.map(function (pairId) {
-                                    const pair = organizationPairs[pairId];
+                            <div className="p-3" />
 
-                                    return (
-                                        <RadioGroup.Option
-                                            className="focus:outline-none ui-checked:bg-gray-500 bg-gray-100 rounded-xl ui-checked:text-white cursor-pointer ui-active:ring-4 ui-active:ring-gray-400 shadow-md"
-                                            value={
-                                                {
-                                                    organizationPairCompositeId: `${pair[0].organizationId}-${pairId}`,
-                                                    pairId: Number(pairId),
-                                                    organizationId: Number(
-                                                        pair[0].organizationId
-                                                    ),
-                                                } as OrganizationPair
-                                            }
-                                        >
-                                            <div className="p-1">
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    fill="none"
-                                                    viewBox="0 0 24 24"
-                                                    strokeWidth={1.5}
-                                                    stroke="currentColor"
-                                                    className="w-6 h-6 hidden ui-not-checked:block"
-                                                >
-                                                    <path
-                                                        strokeLinecap="round"
-                                                        strokeLinejoin="round"
-                                                        d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                                                    />
-                                                </svg>
+                            <RadioGroup
+                                by={"organizationPairCompositeId"}
+                                onChange={function (pair: OrganizationPair) {
+                                    setOrganizationPairs(function (prevPairs) {
+                                        return {
+                                            ...prevPairs,
+                                            [organizationName]: pair,
+                                        };
+                                    });
+                                }}
+                            >
+                                <div className="grid grid-cols-2 gap-5">
+                                    {pairIds.map(function (pairId) {
+                                        const pair = organizationPairs[pairId];
 
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    viewBox="0 0 24 24"
-                                                    fill="currentColor"
-                                                    className="w-6 h-6 hidden ui-checked:block"
-                                                >
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm3 10.5a.75.75 0 000-1.5H9a.75.75 0 000 1.5h6z"
-                                                        clipRule="evenodd"
-                                                    />
-                                                </svg>
-                                            </div>
+                                        return (
+                                            <RadioGroup.Option
+                                                className="focus:outline-none ui-checked:bg-gray-500 bg-gray-100 rounded-xl ui-checked:text-white cursor-pointer ui-active:ring-4 ui-active:ring-gray-400 shadow-md"
+                                                value={
+                                                    {
+                                                        organizationPairCompositeId: `${pair[0].organizationId}-${pairId}`,
+                                                        pairId: Number(pairId),
+                                                        organizationId: Number(
+                                                            pair[0]
+                                                                .organizationId
+                                                        ),
+                                                    } as OrganizationPair
+                                                }
+                                            >
+                                                <div className="p-1">
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        fill="none"
+                                                        viewBox="0 0 24 24"
+                                                        strokeWidth={1.5}
+                                                        stroke="currentColor"
+                                                        className="w-6 h-6 hidden ui-not-checked:block"
+                                                    >
+                                                        <path
+                                                            strokeLinecap="round"
+                                                            strokeLinejoin="round"
+                                                            d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                                                        />
+                                                    </svg>
 
-                                            <div className="flex flex-col gap-3 ui-checked:divide-gray-600 divide-y">
-                                                {pair.map(function (member) {
-                                                    return (
-                                                        <div className="p-5">
-                                                            <h3 className="font-semibold">
-                                                                {
-                                                                    member.fullName
-                                                                }
-                                                            </h3>
-                                                            <span className="ui-checked:text-gray-100">
-                                                                {member.position ===
-                                                                "vice_chairman"
-                                                                    ? "Wakil Ketua"
-                                                                    : "Ketua"}
-                                                            </span>
-                                                        </div>
-                                                    );
-                                                })}
-                                            </div>
-                                        </RadioGroup.Option>
-                                    );
-                                })}
-                            </div>
-                        </RadioGroup>
-                    </div>
-                );
-            })}
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        viewBox="0 0 24 24"
+                                                        fill="currentColor"
+                                                        className="w-6 h-6 hidden ui-checked:block"
+                                                    >
+                                                        <path
+                                                            fillRule="evenodd"
+                                                            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm3 10.5a.75.75 0 000-1.5H9a.75.75 0 000 1.5h6z"
+                                                            clipRule="evenodd"
+                                                        />
+                                                    </svg>
+                                                </div>
+
+                                                <div className="flex flex-col gap-3 ui-checked:divide-gray-600 divide-y">
+                                                    {pair.map(function (
+                                                        member
+                                                    ) {
+                                                        return (
+                                                            <div className="p-5">
+                                                                <h3 className="font-semibold">
+                                                                    {
+                                                                        member.fullName
+                                                                    }
+                                                                </h3>
+                                                                <span className="ui-checked:text-gray-100">
+                                                                    {member.position ===
+                                                                    "vice_chairman"
+                                                                        ? "Wakil Ketua"
+                                                                        : "Ketua"}
+                                                                </span>
+                                                            </div>
+                                                        );
+                                                    })}
+                                                </div>
+                                            </RadioGroup.Option>
+                                        );
+                                    })}
+                                </div>
+                            </RadioGroup>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
@@ -311,7 +318,7 @@ export default function Home(
 
     return (
         <div>
-            <div className="flex flex-col px-5">
+            <div className="flex flex-col px-5 max-w-xl m-auto">
                 <div className="p-3" />
 
                 {pageIndex === 1 ? (
@@ -395,7 +402,7 @@ export default function Home(
                     </>
                 ) : null}
 
-                <div className="p-3" />
+                <div className="p-6" />
             </div>
 
             <div className="flex gap-3 justify-center">
@@ -426,6 +433,7 @@ export default function Home(
                             d="M15.75 19.5L8.25 12l7.5-7.5"
                         />
                     </svg>
+                    Previous
                 </button>
 
                 <button
@@ -454,6 +462,7 @@ export default function Home(
                         });
                     }}
                 >
+                    Next
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
