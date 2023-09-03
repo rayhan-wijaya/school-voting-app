f5f1a358e599dc9e60c8e01a6efe63e81bdf5f6f
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-03 22:33:00 +0700

    used hashPassword property in Buffer creation

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -45,7 +45,7 @@ export async function isAuthValid({
                             .digest("hex")
                     );
                     const hashedOriginalPassword = Buffer.from(
-                        firstResult.password
+                        firstResult.hashedPassword
                     );
 
                     return resolve(
