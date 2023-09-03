09d7600f5224bf0bff5522e86020df9cd8c332a3
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-03 22:34:03 +0700

    created validate_password endpoint handler

--- src/pages/api/validate_password.ts
+++ src/pages/api/validate_password.ts
@@ -15,3 +15,40 @@ const getValidatePasswordQuerySchema = z.object({
     }),
     password: z.string(),
 });
+
+async function handleGet(request: NextApiRequest, response: NextApiResponse) {
+    console.log(request.query);
+
+    const parsedQuery = await getValidatePasswordQuerySchema.safeParseAsync(
+        request.query
+    );
+
+    if (!parsedQuery.success) {
+        return response.status(400).json({ error: parsedQuery.error.issues });
+    }
+
+    if (
+        !(await isAuthValid({
+            studentId: parsedQuery.data.studentId,
+            requestedPassword: parsedQuery.data.password,
+        }))
+    ) {
+        return response.status(400).json({ error: "Incorrect password" });
+    }
+
+    return response.status(200).json({ message: "OK" });
+}
+
+export default async function handler(
+    request: NextApiRequest,
+    response: NextApiResponse
+) {
+    switch (request.method) {
+        case "GET":
+            return await handleGet(request, response);
+        default:
+            return response
+                .status(501)
+                .json({ message: "Unimplemented method" });
+    }
+}
