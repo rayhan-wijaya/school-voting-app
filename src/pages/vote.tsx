e7cfe24a9cfb9c22b98f0034d4453a025c00ae2e
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-04 22:58:04 +0700

    created getOrganizationMembers()

--- src/pages/vote.tsx
+++ src/pages/vote.tsx
@@ -8,3 +8,10 @@ import { organizationPairsAtom } from "~/lib/atoms";
 import { env } from "~/lib/env";
 import { type OrganizationMembers } from "~/pages/api/members";
 import { type OrganizationPair } from "~/types/organization";
+
+async function getOrganizationMembers() {
+    const response = await fetch(
+        new URL("/api/members", env.NEXT_PUBLIC_BASE_URL)
+    );
+    return await (response.json() as OrganizationMembers);
+}
