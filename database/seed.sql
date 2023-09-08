ebeceb598f3290827db466cdedc70ff2059e8dde
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-08 19:33:28 +0700

    updated seed insertion

--- database/seed.sql
+++ database/seed.sql
@@ -1,24 +1,10 @@
 INSERT INTO
     `organization_pair` (`organization_id`, `pair_id`)
 VALUES
-    (1, 1),
-    (1, 2),
-    (1, 3),
-
-    (2, 1),
-    (2, 2);
+    (2, 1);
 
 INSERT INTO
-    `organization_member` (`organization_id`, `pair_id`, `nickname`, `full_name`, `position`)
+    `organization_member` (`organization_id`, `pair_id`, `nickname`, `position`, `image_file_name`)
 VALUES
-    (1, 1, 'Rayhan', 'Rayhan Satria Wijaya', 'vice_chairman'),
-    (1, 1, 'Malik', 'Malik Syarif Akbar', 'chairman'),
-    (1, 2, 'Akesh', 'Gede Akesh', 'vice_chairman'),
-    (1, 2, 'Daffa', 'Daffa Prawono', 'chairman'),
-    (1, 3, 'Akesh', 'Gede Akesh', 'vice_chairman'),
-    (1, 3, 'Daffa', 'Daffa Prawono', 'chairman'),
-
-    (2, 1, 'Rayhan', 'Rayhan Satria Wijaya', 'vice_chairman'),
-    (2, 1, 'Malik', 'Malik Syarif Akbar', 'chairman'),
-    (2, 2, 'Akesh', 'Gede Akesh', 'vice_chairman'),
-    (2, 2, 'Daffa', 'Daffa Prawono', 'chairman');
+    (2, 1, 'Syahira', 'chairman', 'syahira.jpg'),
+    (2, 1, 'Haekal', 'vice_chairman', 'haekal.jpg');
