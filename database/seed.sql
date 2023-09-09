b44682ec42010a3f961b428b55be78a8c98534f8
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-09 10:30:33 +0700

    added another organization pair

--- database/seed.sql
+++ database/seed.sql
@@ -1,10 +1,14 @@
 INSERT INTO
     `organization_pair` (`organization_id`, `pair_id`)
 VALUES
-    (2, 1);
+    (2, 1),
+    (2, 2);
 
 INSERT INTO
     `organization_member` (`organization_id`, `pair_id`, `nickname`, `position`, `image_file_name`)
 VALUES
     (2, 1, 'Syahira', 'chairman', 'syahira.jpg'),
-    (2, 1, 'Haekal', 'vice_chairman', 'haekal.jpg');
+    (2, 1, 'Haekal', 'vice_chairman', 'haekal.jpg'),
+
+    (2, 2, 'Jasmin', 'chairman', 'jasmin.jpg'),
+    (2, 2, 'Neora', 'vice_chairman', 'neora.jpg');
