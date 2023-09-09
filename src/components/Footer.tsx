1321b71a1304914df1d8891d2d9b72cc8c3bab65
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-09 11:13:16 +0700

    created <Footer /> component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div className="text-center py-9 text-gray-400">
+            Rayhan Satria (8A) © 2023
+        </div>
+    );
+}
+
+export default Footer;
