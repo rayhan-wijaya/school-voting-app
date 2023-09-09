d66e9a5594f246ac9632e34bed44b2a05579c229
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-09 22:59:02 +0700

    lightened ring color

--- src/pages/vote.tsx
+++ src/pages/vote.tsx
@@ -159,7 +159,7 @@ function VotePage() {
 
                                     return (
                                         <RadioGroup.Option
-                                            className="focus:outline-none ui-checked:bg-sky-500 bg-sky-100 rounded-xl ui-checked:text-white cursor-pointer ui-active:ring-4 ui-active:ring-sky-200"
+                                            className="focus:outline-none ui-checked:bg-sky-500 bg-sky-100 rounded-xl ui-checked:text-white cursor-pointer ui-active:ring-4 ui-active:ring-sky-100"
                                             key={`${pair[0].organizationId}-${pairId}`}
                                             value={
                                                 {
