382a197a491b04abd02ea98d944efb5e3ef13068
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-09 23:03:51 +0700

    restyled <Header />

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 function Header() {
     return (
-        <div>
+        <div className="flex items-center justify-center">
             <div className="font-semibold p-3 m-3 rounded-lg bg-sky-600 shadow-md text-white">
-                <img className="rounded-lg" src="/backgrounds/header.png" />
+                <img className="rounded-lg h-40" src="/backgrounds/header.png" />
             </div>
         </div>
     );
