63076593a7013a66dbd4a8088385a24047833178
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-09 21:31:59 +0700

    included members for new pairs

--- database/seed.sql
+++ database/seed.sql
@@ -11,6 +11,13 @@ VALUES
 INSERT INTO
     `organization_member` (`organization_id`, `pair_id`, `nickname`, `position`, `image_file_name`)
 VALUES
+    (1, 1, 'Maylaff', 'chairman', 'maylaff.jpg'),
+    (1, 1, 'Rafa', 'vice_chairman', 'rafa.jpg'),
+    (1, 2, 'Flo', 'chairman', 'flo.jpg'),
+    (1, 2, 'Arhael', 'vice_chairman', 'arhael.jpg'),
+    (1, 3, 'Tyas', 'chairman', 'tyas.jpg'),
+    (1, 3, 'Anasya', 'vice_chairman', 'anasya.jpg'),
+
     (2, 1, 'Syahira', 'chairman', 'syahira.jpg'),
     (2, 1, 'Haekal', 'vice_chairman', 'haekal.jpg'),
     (2, 2, 'Jasmin', 'chairman', 'jasmin.jpg'),
