275a9236a968de735481eebf4df7101bc9bdb12d
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-09 11:13:24 +0700

    added <Footer /> to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from "next/app";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Header from "~/components/Header";
+import Footer from "~/components/Footer";
 
 const client = new QueryClient();
 
@@ -11,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <QueryClientProvider client={client}>
             <Header />
             <Component {...pageProps} />
+            <Footer />
         </QueryClientProvider>
     );
 }
