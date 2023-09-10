a25b19d8576b79992fb690758d7134be65deb7a6
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-10 17:58:34 +0700

    defined adminSchema in auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,8 @@
 import { z } from "zod";
 import { database } from "../lib/database";
 import { timingSafeEqual } from "crypto";
+
+const adminSchema = z.object({
+    username: z.string(),
+    hashedPassword: z.string(),
+});
