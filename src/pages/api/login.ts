a40b69f6a2a16df170617c900a482b5568c95c9d
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-10 20:12:14 +0700

    removed import for zod

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -2,7 +2,6 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { database } from "~/lib/database";
 import { validateCredentials } from "~/lib/auth";
 import { createId } from "~/lib/cuid";
-import { z } from "zod";
 
 function createSession({
     username,
