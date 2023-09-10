ea323858bf19cb6840ce52e093b1df3cf91aaa14
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-10 17:58:42 +0700

    created validateAuthToken() function

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -6,3 +6,56 @@ const adminSchema = z.object({
     username: z.string(),
     hashedPassword: z.string(),
 });
+
+export function validateAuthToken(authToken: string) {
+    const [username, hashedPassword] = authToken.split(":");
+
+    return new Promise<boolean>(function (resolve, reject) {
+        database.pool.getConnection(function (error, connection) {
+            if (error) {
+                return reject(error);
+            }
+
+            connection.query(
+                {
+                    sql: `
+                        SELECT
+                            username,
+                            hashed_password as hashedPassword
+                        FROM admin
+                        WHERE username = ?;
+                    `,
+                    values: [username],
+                },
+                async function (error, results) {
+                    if (error) {
+                        return reject(error);
+                    }
+
+                    const adminsResult = await z
+                        .array(adminSchema.optional())
+                        .safeParseAsync(results);
+
+                    if (!adminsResult.success) {
+                        return reject("Failed to validate admin results");
+                    }
+
+                    if (!adminsResult.data[0]) {
+                        return reject("Failed to get an admin from DB");
+                    }
+
+                    const adminFromDb = adminsResult.data[0];
+
+                    return resolve(
+                        timingSafeEqual(
+                            Buffer.from(adminFromDb.hashedPassword),
+                            Buffer.from(hashedPassword)
+                        )
+                    );
+                }
+            );
+
+            return connection.release();
+        });
+    });
+}
