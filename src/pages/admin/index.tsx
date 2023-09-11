5ee80206097177620e8c86c3565122f4b4e20074
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-11 19:21:41 +0700

    created initial Admin component

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -10,3 +10,56 @@ async function fetchVotingResults() {
 
     return json;
 }
+
+function Admin() {
+    const { data: votingResults } = useQuery({
+        queryKey: ["voting-results"],
+        queryFn: fetchVotingResults,
+    });
+
+    return (
+        <>
+            <h1 className="text-center font-semibold text-lg">Voting Results</h1>
+
+            <div className="p-3" />
+
+            <div className="flex flex-col gap-8">
+                {(votingResults ? Object.keys(votingResults) : []).map(
+                    function (organizationName) {
+                        const organizationVotingResults =
+                            votingResults?.[organizationName];
+
+                        return (
+                            <div>
+                                <h2
+                                    className="text-center font-semibold"
+                                    dangerouslySetInnerHTML={{
+                                        __html: organizationName,
+                                    }}
+                                />
+
+                                <div className="p-3" />
+
+                                <div className="flex flex-wrap gap-3 items-center justify-center">
+                                    {organizationVotingResults
+                                        ? organizationVotingResults.map(
+                                              function (result) {
+                                                  return (
+                                                      <div className="p-3 bg-sky-100 rounded-xl">
+                                                          <h3>{result.name}</h3>
+                                                      </div>
+                                                  );
+                                              }
+                                          )
+                                        : null}
+                                </div>
+                            </div>
+                        );
+                    }
+                )}
+            </div>
+        </>
+    );
+}
+
+export default Admin;
