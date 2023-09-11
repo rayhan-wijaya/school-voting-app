54100a072b6807a7299bdaa3cd1d95e07bf80b08
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-11 10:32:32 +0700

    imported necessary things in /results endpoint

---
+++ src/pages/api/results.ts
@@ -0,0 +1,5 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+import { database } from "~/lib/database";
+import { filterSuccesses } from "~/lib/promises";
+import { voteSchema } from "~/lib/schemas";
