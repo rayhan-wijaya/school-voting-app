ef31a9529ac41aa1dcf1f77054879aa59bc9fb42
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-11 10:33:00 +0700

    declared getVotes() function

--- src/pages/api/results.ts
+++ src/pages/api/results.ts
@@ -5,3 +5,35 @@ import { filterSuccesses } from "~/lib/promises";
 import { voteSchema } from "~/lib/schemas";
 
 type Vote = z.infer<typeof voteSchema>;
+
+async function getVotes() {
+    return new Promise<Vote[]>(function (resolve, reject) {
+        database.pool.getConnection(function (error, connection) {
+            if (error) {
+                return reject(error);
+            }
+
+            connection.query(
+                {
+                    sql: "",
+                    values: [],
+                },
+                async function (error, results) {
+                    if (error) {
+                        return reject(error);
+                    }
+
+                    const votesResult = await z
+                        .array(voteSchema)
+                        .safeParseAsync(results);
+
+                    if (!votesResult.success) {
+                        return reject("Error while parsing result from db");
+                    }
+
+                    return resolve(votesResult.data);
+                }
+            );
+        });
+    });
+}
