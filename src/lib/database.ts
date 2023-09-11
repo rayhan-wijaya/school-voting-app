4b144a67a2c3a476a6e9262f0f478a2cf451c099
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-11 18:40:58 +0700

    created getFormattedOrganizationName() function

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -10,3 +10,37 @@ export const database = {
         database: env.DATABASE_NAME,
     }),
 };
+
+const cachedOrganizationNames = new Map<number, string>();
+
+function getFormattedOrganizationName({
+    id,
+    connection,
+    isFromCache = false,
+}: {
+    id: number;
+    connection: PoolConnection;
+    isFromCache: boolean;
+}) {
+    if (isFromCache && cachedOrganizationNames.has(id)) {
+        return cachedOrganizationNames.get(id);
+    }
+
+    return new Promise<string>(function (resolve, reject) {
+        connection.query(
+            {
+                sql: "SELECT name, full_name as fullName FROM organization where id = ?",
+                values: [id],
+            },
+            function (error, results, _fields) {
+                if (error) {
+                    return reject(error);
+                }
+
+                return resolve(
+                    `${results?.[0]?.fullName}<br>(${results?.[0]?.name})`
+                );
+            }
+        );
+    });
+}
