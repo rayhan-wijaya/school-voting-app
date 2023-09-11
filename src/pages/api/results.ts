558538e9f1a23422721e275c3a3190ba63c72ec1
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-11 10:34:16 +0700

    removed import for filterSuccesses

--- src/pages/api/results.ts
+++ src/pages/api/results.ts
@@ -1,7 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { z } from "zod";
 import { database } from "~/lib/database";
-import { filterSuccesses } from "~/lib/promises";
 import { voteSchema } from "~/lib/schemas";
 
 type Vote = z.infer<typeof voteSchema>;
