43823400db0438403ebc7c2744f392722ea6e70f
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-11 10:32:49 +0700

    declared Vote type

--- src/pages/api/results.ts
+++ src/pages/api/results.ts
@@ -3,3 +3,5 @@ import { z } from "zod";
 import { database } from "~/lib/database";
 import { filterSuccesses } from "~/lib/promises";
 import { voteSchema } from "~/lib/schemas";
+
+type Vote = z.infer<typeof voteSchema>;
