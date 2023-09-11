131b420a57aa1a752d5745b0d11bb2af075d14ad
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-11 19:16:11 +0700

    defined fetchVotingResults()

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,3 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { env } from "~/lib/env";
 import type { VotingResults } from "../api/admin/results";
+
+async function fetchVotingResults() {
+    const response = await fetch(
+        new URL("/api/admin/results", env.NEXT_PUBLIC_BASE_URL)
+    );
+    const json = (await response.json()) as VotingResults;
+
+    return json;
+}
