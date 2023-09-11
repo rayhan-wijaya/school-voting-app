d1446020058190b1838d71872657a202a1b5af34
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-11 22:17:34 +0700

    formatted <Header /> component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,10 +7,15 @@ function Header() {
     const [currentTab, setCurrentTab] = useState<"voting" | "admin">();
     const [isAdmin, setIsAdmin] = useState(false);
 
-    useEffect(function () {
-        setIsAdmin(document.cookie.indexOf("admin_session_token=") !== -1);
-        setCurrentTab(router.pathname.startsWith("/admin") ? "admin" : "voting");
-    }, [router]);
+    useEffect(
+        function () {
+            setIsAdmin(document.cookie.indexOf("admin_session_token=") !== -1);
+            setCurrentTab(
+                router.pathname.startsWith("/admin") ? "admin" : "voting"
+            );
+        },
+        [router]
+    );
 
     return (
         <>
@@ -28,24 +33,28 @@ function Header() {
                     <div className="p-3" />
 
                     <div className="flex items-center justify-center">
-                    <div className="inline-flex rounded-xl gap-3 items-center justify-center p-2 bg-gray-50">
-                        <Link
-                            className={`${
-                                currentTab === "voting" ? "bg-sky-100 shadow-md" : "hover:bg-gray-100"
-                            } py-3 px-5 rounded-xl`}
-                            href="/"
-                        >
-                            Voting Page
-                        </Link>
-                        <Link
-                            className={`${
-                                currentTab === "admin" ? "bg-sky-100 shadow-md" : "hover:bg-gray-100"
-                            } py-3 px-5 rounded-xl`}
-                            href="/admin"
-                        >
-                            Admin Dashboard
-                        </Link>
-                    </div>
+                        <div className="inline-flex rounded-xl gap-3 items-center justify-center p-2 bg-gray-50">
+                            <Link
+                                className={`${
+                                    currentTab === "voting"
+                                        ? "bg-sky-100 shadow-md"
+                                        : "hover:bg-gray-100"
+                                } py-3 px-5 rounded-xl`}
+                                href="/"
+                            >
+                                Voting Page
+                            </Link>
+                            <Link
+                                className={`${
+                                    currentTab === "admin"
+                                        ? "bg-sky-100 shadow-md"
+                                        : "hover:bg-gray-100"
+                                } py-3 px-5 rounded-xl`}
+                                href="/admin"
+                            >
+                                Admin Dashboard
+                            </Link>
+                        </div>
                     </div>
 
                     <div className="p-3" />
