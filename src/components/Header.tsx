df3acae6d43e79babab590157e322b1f08e0baa7
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-11 21:33:32 +0700

    created admin tabs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,58 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import { z } from "zod";
+
 function Header() {
+    const router = useRouter();
+    const [currentTab, setCurrentTab] = useState<"voting" | "admin">();
+
+    useEffect(function () {
+        const { tab } = router.query;
+
+        const tabResult = z
+            .enum(["voting", "admin"])
+            .safeParse(tab);
+
+        if (tabResult.success) {
+            setCurrentTab(tabResult.data);
+        }
+    }, [router]);
+
     return (
-        <div className="flex items-center justify-center">
-            <div className="font-semibold p-3 m-3 rounded-lg bg-sky-600 shadow-md text-white">
-                <img className="rounded-lg h-40" src="/backgrounds/header.png" />
+        <>
+            <div className="flex items-center justify-center">
+                <div className="font-semibold p-3 m-3 rounded-lg bg-sky-600 shadow-md text-white">
+                    <img
+                        className="rounded-lg h-40"
+                        src="/backgrounds/header.png"
+                    />
+                </div>
             </div>
-        </div>
+
+            {currentTab ? (
+                <>
+                    <div className="flex gap-3 items-center justify-center p-8">
+                        <Link
+                            className={`${
+                                currentTab === "voting" ? "bg-sky-100" : ""
+                            } p-3 rounded-xl border-2 border-sky-100`}
+                            href="/"
+                        >
+                            Voting Page
+                        </Link>
+                        <Link
+                            className={`${
+                                currentTab === "admin" ? "bg-sky-100" : ""
+                            } p-3 rounded-xl border-2 border-sky-100`}
+                            href="/admin"
+                        >
+                            Admin Dashboard
+                        </Link>
+                    </div>
+                </>
+            ) : null}
+        </>
     );
 }
 
