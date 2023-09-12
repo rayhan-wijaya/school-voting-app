8aab42106a7d7a0f4024a524b2e6dcf2163aa2a8
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-12 21:33:19 +0700

    refactored validateCredentials() to accept connection

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { database } from "../lib/database";
 import { timingSafeEqual } from "crypto";
+import type { PoolConnection } from "mysql";
 
 const adminSchema = z.object({
     username: z.string(),
@@ -10,57 +11,51 @@ const adminSchema = z.object({
 export function validateCredentials({
     username,
     hashedPassword,
+    connection,
 }: {
     username: string;
     hashedPassword: string;
+    connection: PoolConnection;
 }) {
     return new Promise<boolean>(function (resolve, reject) {
-        database.pool.getConnection(function (error, connection) {
-            if (error) {
-                return reject(error);
-            }
-
-            connection.query(
-                {
-                    sql: `
-                        SELECT
-                            username,
-                            hashed_password as hashedPassword
-                        FROM admin
-                        WHERE username = ?;
-                    `,
-                    values: [username],
-                },
-                async function (error, results) {
-                    if (error) {
-                        return reject(error);
-                    }
-
-                    const adminsResult = await z
-                        .array(adminSchema.optional())
-                        .safeParseAsync(results);
-
-                    if (!adminsResult.success) {
-                        return resolve(false);
-                    }
-
-                    if (!adminsResult.data[0]) {
-                        return resolve(false);
-                    }
-
-                    const adminFromDb = adminsResult.data[0];
-
-                    return resolve(
-                        timingSafeEqual(
-                            Buffer.from(adminFromDb.hashedPassword),
-                            Buffer.from(hashedPassword)
-                        )
-                    );
+        connection.query(
+            {
+                sql: `
+                    SELECT
+                        username,
+                        hashed_password as hashedPassword
+                    FROM admin
+                    WHERE username = ?;
+                `,
+                values: [username],
+            },
+            async function (error, results) {
+                if (error) {
+                    return reject(error);
                 }
-            );
 
-            return connection.release();
-        });
+                const adminsResult = await z
+                    .array(adminSchema.optional())
+                    .safeParseAsync(results);
+
+                if (!adminsResult.success) {
+                    return resolve(false);
+                }
+
+                if (!adminsResult.data[0]) {
+                    return resolve(false);
+                }
+
+                const adminFromDb = adminsResult.data[0];
+
+                return resolve(
+                    timingSafeEqual(
+                        Buffer.from(adminFromDb.hashedPassword),
+                        Buffer.from(hashedPassword)
+                    )
+                );
+            }
+        );
     });
 }
 
