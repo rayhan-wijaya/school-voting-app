41119bce4c0261e8915dcb8bb50a654f380836c2
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-12 23:23:05 +0700

    removed previous button in student id page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,27 +74,6 @@ function StudentDetailsPage() {
             <div className="flex gap-3 justify-center">
                 <button
                     className="flex gap-3 bg-gray-200 rounded-xl p-5 py-3 items-center disabled:bg-gray-50 disabled:text-gray-300 hover:bg-gray-300 transition-colors"
-                    disabled={true}
-                >
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth="1.5"
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M15.75 19.5L8.25 12l7.5-7.5"
-                        />
-                    </svg>
-                    <span className="hidden sm:block">Previous</span>
-                </button>
-
-                <button
-                    className="flex gap-3 bg-gray-200 rounded-xl p-5 py-3 items-center disabled:bg-gray-50 disabled:text-gray-300 hover:bg-gray-300 transition-colors"
                     disabled={!studentId}
                     onClick={async function () {
                         const studentVoteStatusResponse = await fetch(
