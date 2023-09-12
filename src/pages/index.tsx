71e9c5ebcfa605e2b8cebbdd8dd35ae062e93496
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-09-12 20:40:01 +0700

    stylized you already voted warning box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,24 @@ function StudentDetailsPage() {
 
             {isWarningVisible ? (
                 <>
-                    <div className="text-center">You already voted</div>
+                    <div className="text-center bg-yellow-200 p-3 mx-3 rounded-lg flex items-center justify-center gap-3">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+                            />
+                        </svg>
+
+                        You already voted
+                    </div>
                     <div className="p-3" />
                 </>
             ) : null}
