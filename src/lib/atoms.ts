1ff24d67291d61d249b1a1a4eb0db5ee79d0952e
rayhan-wijaya <rayhanwijaya210@gmail.com>
2023-10-14 21:08:55 +0700

    changed type of studentIdAtom to string

--- src/lib/atoms.ts
+++ src/lib/atoms.ts
@@ -1,5 +1,5 @@
 import { atom } from "jotai";
 import { type OrganizationPairs } from "~/types/organization";
 
-export const studentIdAtom = atom<number | undefined>(undefined);
+export const studentIdAtom = atom<string | undefined>(undefined);
 export const organizationPairsAtom = atom<OrganizationPairs>({});
